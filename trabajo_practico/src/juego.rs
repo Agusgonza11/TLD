623ff30af4f4139341b95ab22629dc3fee21f6e7
Agustin Gonzalez <aguselgonza@gmail.com>
2024-05-10 14:27:22 -0300

    realizar acciones

--- trabajo_practico/src/juego.rs
+++ trabajo_practico/src/juego.rs
@@ -3,14 +3,14 @@ use crate::{custom_error::CustomError, mapa::Mapa, jugador::Jugador };
 pub struct Juego{
     pub mapa: Mapa,
     pub jugadores: Vec<Jugador>,
-    pub turno: i32,
+    pub turno: usize,
 }
 
 impl Juego{
     pub fn new(numero_jugadores: i32) -> Juego {
         let mapa = Mapa::new();
         let mut jugadores = Vec::new();
-        for t in 1..numero_jugadores{
+        for t in 1..=numero_jugadores{
             jugadores.push(Jugador::new(t));
         }
         let turno = 0;
@@ -18,13 +18,32 @@ impl Juego{
     }
 
     pub fn iniciar_juego(&mut self) -> Result<(),CustomError>{
+        self.imprimir_acciones();
         self.mapa.imprimir_tablero();
+
+        while !self.finalizo() {
+            let jugador_actual = &mut self.jugadores[self.turno];
+            jugador_actual.turno();
+            self.mapa.imprimir_tablero();
+
+            self.turno = (self.turno + 1) % self.jugadores.len();
+        }
         Ok(())
+    }
+
+    fn finalizo(&self) -> bool {
+        self.jugadores.len() <= 1
+    }
+
+    fn imprimir_acciones(&self) {
+        println!("Realice una accion: ");
+        println!("Puede moverse: (m)");
+        println!("Puede atacar: (a)");
+        println!("Puede abrir la tienda: (t)");
     }
     
     pub fn agregar_jugador(&mut self, jugador: Jugador) {
         self.jugadores.push(jugador);
-        
     }
 
 }
--- trabajo_practico/src/jugador.rs
+++ trabajo_practico/src/jugador.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 pub struct Jugador{
     pub id: i32
 }
@@ -7,4 +9,16 @@ impl Jugador{
         Jugador { id }
     }
 
+    pub fn turno(&mut self) {
+        let mut accion = String::new();
+        io::stdin().read_line(&mut accion)
+            .expect("Error al leer la entrada");
+    
+        match accion.trim() {
+            "m" => println!("te moviste"),
+            "a" => println!("atacaste"),
+            "t" => println!("tienda"),
+            _ => println!("Ingrese una accion valida"),
+        }
+    }
 }
